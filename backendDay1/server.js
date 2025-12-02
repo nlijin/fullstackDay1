4473acdb0d82555e04c24a30e3ab7a331d9e92b0
nlijin <nlijinraj@gmail.com>
2025-12-03 00:48:19 +0530

    added few more endpoints to the server.js

--- backendDay1/server.js
+++ backendDay1/server.js
@@ -1,9 +1,21 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
+const port = 3000;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from the the backend server" });
 });
 
-app.listen(3000, () => console.log("server running on port 3000"));
+// you can also use HTML response as in below code;
+app.get("/about", (req, res) => {
+  res.send("<h2>Hello from backend about endpoint</h2>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send(
+    "<div><h2>this is using multiple tags</h2><h2>my phone number is 123455667</h2></div>"
+  );
+});
+
+app.listen(port, () => console.log(`server running on port ${port}`));
